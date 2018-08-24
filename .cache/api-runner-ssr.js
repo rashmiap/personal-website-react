5707139644635d906c594a9dc3de5c9b47591524
Rashmi <rashmiap.10@gmail.com>
2018-08-24 15:59:48 +0530

    favicon added

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,6 +1,9 @@
 var plugins = [{
       plugin: require('/Users/rashmi/Github/personal-website-react/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
+    },{
+      plugin: require('/Users/rashmi/Github/personal-website-react/node_modules/gatsby-plugin-styled-components/gatsby-ssr'),
+      options: {"plugins":[]},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [

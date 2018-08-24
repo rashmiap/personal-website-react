a24925bb000d3d2b526b363dbe9a3810954476c3
Rashmi <rashmiap.10@gmail.com>
2018-08-24 14:32:05 +0530

    gatsby styled component plugin added package.json updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Rashmi AP',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components'
+  ],
 }

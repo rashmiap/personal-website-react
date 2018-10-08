9f6fcc27acd181f1d5555f3620e2bd371b26c369
Rashmi <rashmiap.10@gmail.com>
2018-10-08 17:26:40 +0530

    index file updated changed to new design

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,50 +7,62 @@ import gmail from '../assets/images/gmail.svg'
 import github from '../assets/images/github.svg'
 
 const UserWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  height: inherit;
-  font-family: fantasy;
-  max-width: 70%;
-  margin: 0 auto;
+  margin: 10% auto;
 `;
 const UserTitle = styled.h1`
-  font-family: fantasy;
-`;
-const UserDescription = styled.p`
-  border-bottom: 1px solid;
-  padding-bottom: 20px;
-`;
-const SocialWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  a{
-    margin-right: 20px;
-    opacity: 0.5;
+  font-size: 6em;
+  font-weight: bold;
+  color: #ffffff;
+  span{
+    color: #7fa1e8;
   }
-  a:hover{
-    opacity: 1;
+`;
+const UserDescription = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-evenly;
+  p{
+    color: #ffffff;
+    max-width: 90%;
+    font-weight: 300;
+    font-size: 26px;
+    line-height: 1.3;
+    margin-bottom: 15%;
   }
   img{
-    width: 40px;
+    max-width: 50%;
   }
 `;
+const DownloadButton = styled.div`
+  width: 298px;
+  height: 71px;
+  background-color: rgba(188, 188, 188, 0.1);
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: CENTER;
+  color: #ffffff;
+  cursor: pointer;
+  :hover{
+    color: #7fa1e8;
+  }
+`
+
 
 const IndexPage = () => (
   <UserWrapper>
-    <img src={RashSelfIcon} alt="rashmi self" />
-    <div>
-      <UserTitle>Rashmi ap</UserTitle>
-      <UserDescription>Hi! I'm a frontend developer from Bangalore, India. Employed at ZoojooBE. </UserDescription>
-    </div>
-    <SocialWrapper>
-      <a href="https://github.com/rashmiap"><img src={github} /></a>
-      <a href="https://www.linkedin.com/in/rashmi-ap-601879b4/"><img src={linkedIn} /></a>
-      <a href="mailto:rashmiap.10@gmail.com?Subject=SayHi%20again" target="_top"><img src={gmail} /></a>
-    </SocialWrapper>
+    <UserTitle>Rashmi <span>AP</span></UserTitle>
+    <UserDescription>
+      <div>
+        <p>
+        Hello there, welcome to my portfolio. I am a
+        front end developer, visual artist and an animal lover
+        based out of Bangalore.
+        </p>
+        <DownloadButton>Download Resume</DownloadButton>
+      </div>
+      <img src={RashSelfIcon} alt="rashmi self" />
+    </UserDescription>
   </UserWrapper>
 )
 

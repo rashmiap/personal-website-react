2d3f0be808dac21e8d7e18629cfe922c85340217
Rashmi <rashmiap.10@gmail.com>
2018-10-10 17:24:47 +0530

    projects json updated

--- src/data/ProProjects.js
+++ src/data/ProProjects.js
@@ -1,32 +1,44 @@
-import movie from '../assets/images/projects/movie.svg'
+import pregnancy from '../assets/images/projects/pregnancy.svg'
+import like from '../assets/images/projects/social.svg'
+import dare from '../assets/images/projects/dare.svg'
+import journey from '../assets/images/projects/journey.svg'
 
 export default [
     {
         id: 1,
-        title: "ZoojooBE social stream",
-        icon: movie,
-        description: "Social feed is primar",
+        title: "ZoojooBE Social Stream",
+        icon: like,
+        description: "Social feed is a feature on the ZoojooBE platform that leverages on the social behavior within the corporate.It allows users on the platform to share media that keeps them motivated to do a certain task. Like all other social feeds, ZoojooBE Stream also has social elements like liking, sharing, commenting, subscribing to a particular post and so on. Developed using ReactJS and draft-js-plugins, a react plugin architecture for Facebook's Draft.js which includes emojis, mentions and stickers. ",
         githubPath: null,
         demoPath: null,
-        techUsed: "React, react-dropzone, Draft.js"
+        techUsed: "React, react-dropzone, draft-js-plugins"
     },
     {
         id: 2,
-        title: "Music Artist Search App",
-        icon: movie,
-        description: "This is a small and simple artist search app developed in ReactJS using the audioDB api.",
-        githubPath: "https://github.com/rashmiap/audioDB-react-app",
-        demoPath: "https://audiodb-artist-searchapp.surge.sh",
-        techUsed: "React, Material-UI, theaudiodb"
+        title: "ZoojooBE Dare",
+        icon: dare,
+        description: "Dare is a feature on the ZoojooBE platform developed in ReactJS that primarily focuses on the competitive behavior of the users. Two users compete with each other to complete a particular healthy task. Meanwhile other users within the corporate can motivate the user using the cheer function.",
+        githubPath: null,
+        demoPath: null,
+        techUsed: "React, Material-UI"
     },
     {
         id: 3,
-        title: "Bulls and Cows React game",
-        icon: movie,
-        description: "Bulls and Cows is an old code breaking mind game.The goal of this game is to guess a 4-digit number within the least number of attempts possible. With every guess, you will get a feedback indicating how many Bulls and Cows you got with the guess.",
-        githubPath: "https://github.com/rashmiap/bulls-cows-react-game",
-        demoPath: "https://bulls-cows-game.surge.sh",
-        techUsed: "React, styled-components"
+        title: "ZoojooBE Journey",
+        icon: journey,
+        description: "Journey feature on the ZoojooBE platform is a set of curated activities that are a set of micro-tasks that a user has to form to achieve a bigger goal like forming a complex habit. Journey feature has multiple levels which changes based on user activity. The whole task list is customised based on user activity.",
+        githubPath: null,
+        demoPath: null,
+        techUsed: "React, Material-UI"
+    },
+    {
+        id: 4,
+        title: "ZoojooBE Pregnancy module",
+        icon: pregnancy,
+        description: "Maternity module was primarily designed for the women within a corporate. The feature takes into account the user's due date based on which it recommends articles and relavent mindfullness tracks. This feature helps them in staying healthy throughout the process.",
+        githubPath: null,
+        demoPath: null,
+        techUsed: "React, Material-UI, React-Sound-Manager"
     },
 
 ];

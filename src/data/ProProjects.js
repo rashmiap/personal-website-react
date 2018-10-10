01c4e45ddc996e1229554eb31f1ca0bd8ff67e71
Rashmi <rashmiap.10@gmail.com>
2018-10-10 16:11:47 +0530

    json added hobby and company projects

---
+++ src/data/ProProjects.js
@@ -0,0 +1,32 @@
+import movie from '../assets/images/projects/movie.svg'
+
+export default [
+    {
+        id: 1,
+        title: "ZoojooBE social stream",
+        icon: movie,
+        description: "Social feed is primar",
+        githubPath: null,
+        demoPath: null,
+        techUsed: "React, react-dropzone, Draft.js"
+    },
+    {
+        id: 2,
+        title: "Music Artist Search App",
+        icon: movie,
+        description: "This is a small and simple artist search app developed in ReactJS using the audioDB api.",
+        githubPath: "https://github.com/rashmiap/audioDB-react-app",
+        demoPath: "https://audiodb-artist-searchapp.surge.sh",
+        techUsed: "React, Material-UI, theaudiodb"
+    },
+    {
+        id: 3,
+        title: "Bulls and Cows React game",
+        icon: movie,
+        description: "Bulls and Cows is an old code breaking mind game.The goal of this game is to guess a 4-digit number within the least number of attempts possible. With every guess, you will get a feedback indicating how many Bulls and Cows you got with the guess.",
+        githubPath: "https://github.com/rashmiap/bulls-cows-react-game",
+        demoPath: "https://bulls-cows-game.surge.sh",
+        techUsed: "React, styled-components"
+    },
+
+];

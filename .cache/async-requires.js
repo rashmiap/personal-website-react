5fc7b5ebb0247904cfa4c705878234d2f4103cd8
Rashmi <rashmiap.10@gmail.com>
2018-10-11 12:24:52 +0530

    generated files

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -6,7 +6,8 @@ exports.components = {
   "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/rashmi/Github/personal-website-react/src/pages/404.js"),
   "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/rashmi/Github/personal-website-react/src/pages/index.js"),
   "component---src-pages-contact-js": require("gatsby-module-loader?name=component---src-pages-contact-js!/Users/rashmi/Github/personal-website-react/src/pages/contact.js"),
-  "component---src-pages-projects-js": require("gatsby-module-loader?name=component---src-pages-projects-js!/Users/rashmi/Github/personal-website-react/src/pages/projects.js")
+  "component---src-pages-projects-js": require("gatsby-module-loader?name=component---src-pages-projects-js!/Users/rashmi/Github/personal-website-react/src/pages/projects.js"),
+  "component---src-pages-recommendation-js": require("gatsby-module-loader?name=component---src-pages-recommendation-js!/Users/rashmi/Github/personal-website-react/src/pages/recommendation.js")
 }
 
 exports.json = {
@@ -16,7 +17,8 @@ exports.json = {
   "index.json": require("gatsby-module-loader?name=path---index!/Users/rashmi/Github/personal-website-react/.cache/json/index.json"),
   "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/rashmi/Github/personal-website-react/.cache/json/404-html.json"),
   "contact.json": require("gatsby-module-loader?name=path---contact!/Users/rashmi/Github/personal-website-react/.cache/json/contact.json"),
-  "projects.json": require("gatsby-module-loader?name=path---projects!/Users/rashmi/Github/personal-website-react/.cache/json/projects.json")
+  "projects.json": require("gatsby-module-loader?name=path---projects!/Users/rashmi/Github/personal-website-react/.cache/json/projects.json"),
+  "recommendation.json": require("gatsby-module-loader?name=path---recommendation!/Users/rashmi/Github/personal-website-react/.cache/json/recommendation.json")
 }
 
 exports.layouts = {

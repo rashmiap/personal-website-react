b55ce0ed61e56b92cf773b107921977ba6461f38
Rashmi <rashmiap.10@gmail.com>
2018-10-12 12:30:26 +0530

    added CNAME deployed to https://rashmiap.surge.sh/

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,25 +2,23 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/rashmi/Github/personal-website-react/.cache/dev-404-page.js"),
   "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/rashmi/Github/personal-website-react/src/pages/404.js"),
-  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/rashmi/Github/personal-website-react/src/pages/index.js"),
   "component---src-pages-contact-js": require("gatsby-module-loader?name=component---src-pages-contact-js!/Users/rashmi/Github/personal-website-react/src/pages/contact.js"),
+  "component---src-pages-experience-js": require("gatsby-module-loader?name=component---src-pages-experience-js!/Users/rashmi/Github/personal-website-react/src/pages/experience.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/rashmi/Github/personal-website-react/src/pages/index.js"),
   "component---src-pages-projects-js": require("gatsby-module-loader?name=component---src-pages-projects-js!/Users/rashmi/Github/personal-website-react/src/pages/projects.js"),
-  "component---src-pages-recommendation-js": require("gatsby-module-loader?name=component---src-pages-recommendation-js!/Users/rashmi/Github/personal-website-react/src/pages/recommendation.js"),
-  "component---src-pages-experience-js": require("gatsby-module-loader?name=component---src-pages-experience-js!/Users/rashmi/Github/personal-website-react/src/pages/experience.js")
+  "component---src-pages-recommendation-js": require("gatsby-module-loader?name=component---src-pages-recommendation-js!/Users/rashmi/Github/personal-website-react/src/pages/recommendation.js")
 }
 
 exports.json = {
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/rashmi/Github/personal-website-react/.cache/json/layout-index.json"),
-  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/rashmi/Github/personal-website-react/.cache/json/dev-404-page.json"),
   "404.json": require("gatsby-module-loader?name=path---404!/Users/rashmi/Github/personal-website-react/.cache/json/404.json"),
-  "index.json": require("gatsby-module-loader?name=path---index!/Users/rashmi/Github/personal-website-react/.cache/json/index.json"),
-  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/rashmi/Github/personal-website-react/.cache/json/404-html.json"),
   "contact.json": require("gatsby-module-loader?name=path---contact!/Users/rashmi/Github/personal-website-react/.cache/json/contact.json"),
+  "experience.json": require("gatsby-module-loader?name=path---experience!/Users/rashmi/Github/personal-website-react/.cache/json/experience.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/Users/rashmi/Github/personal-website-react/.cache/json/index.json"),
   "projects.json": require("gatsby-module-loader?name=path---projects!/Users/rashmi/Github/personal-website-react/.cache/json/projects.json"),
   "recommendation.json": require("gatsby-module-loader?name=path---recommendation!/Users/rashmi/Github/personal-website-react/.cache/json/recommendation.json"),
-  "experience.json": require("gatsby-module-loader?name=path---experience!/Users/rashmi/Github/personal-website-react/.cache/json/experience.json")
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/rashmi/Github/personal-website-react/.cache/json/404-html.json")
 }
 
 exports.layouts = {
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,23 +7,21 @@ exports.layouts = {
 }
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/rashmi/Github/personal-website-react/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/rashmi/Github/personal-website-react/src/pages/404.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/rashmi/Github/personal-website-react/src/pages/index.js")),
   "component---src-pages-contact-js": preferDefault(require("/Users/rashmi/Github/personal-website-react/src/pages/contact.js")),
+  "component---src-pages-experience-js": preferDefault(require("/Users/rashmi/Github/personal-website-react/src/pages/experience.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/rashmi/Github/personal-website-react/src/pages/index.js")),
   "component---src-pages-projects-js": preferDefault(require("/Users/rashmi/Github/personal-website-react/src/pages/projects.js")),
-  "component---src-pages-recommendation-js": preferDefault(require("/Users/rashmi/Github/personal-website-react/src/pages/recommendation.js")),
-  "component---src-pages-experience-js": preferDefault(require("/Users/rashmi/Github/personal-website-react/src/pages/experience.js"))
+  "component---src-pages-recommendation-js": preferDefault(require("/Users/rashmi/Github/personal-website-react/src/pages/recommendation.js"))
 }
 
 exports.json = {
   "layout-index.json": require("/Users/rashmi/Github/personal-website-react/.cache/json/layout-index.json"),
-  "dev-404-page.json": require("/Users/rashmi/Github/personal-website-react/.cache/json/dev-404-page.json"),
   "404.json": require("/Users/rashmi/Github/personal-website-react/.cache/json/404.json"),
-  "index.json": require("/Users/rashmi/Github/personal-website-react/.cache/json/index.json"),
-  "404-html.json": require("/Users/rashmi/Github/personal-website-react/.cache/json/404-html.json"),
   "contact.json": require("/Users/rashmi/Github/personal-website-react/.cache/json/contact.json"),
+  "experience.json": require("/Users/rashmi/Github/personal-website-react/.cache/json/experience.json"),
+  "index.json": require("/Users/rashmi/Github/personal-website-react/.cache/json/index.json"),
   "projects.json": require("/Users/rashmi/Github/personal-website-react/.cache/json/projects.json"),
   "recommendation.json": require("/Users/rashmi/Github/personal-website-react/.cache/json/recommendation.json"),
-  "experience.json": require("/Users/rashmi/Github/personal-website-react/.cache/json/experience.json")
+  "404-html.json": require("/Users/rashmi/Github/personal-website-react/.cache/json/404-html.json")
 }

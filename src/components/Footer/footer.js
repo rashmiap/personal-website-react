c44675144940977ea2032f53d0fde916dac32c59
Rashmi <rashmiap.10@gmail.com>
2018-10-15 14:18:20 +0530

    dynamic css added to footer Signika-Regular included css update

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,32 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
-const FooterBox = styled.div`
-  margin: 8% auto 0;
-  text-align: CENTER;
-  p{
-    color: #0f1645;
-  }
-  a{
-    color: #7fa1e8;
-    font-weight: bold;
-    text-decoration: none;
-  }
-  @media (max-width: 700px) {
-    bottom: 0;
-    padding: 5% 0 0;
-    margin: 0px -8% -5% -8%;
-    position: absolute;
-    width: 100%;
-    box-sizing: border-box;
-    background-color: #eeeeee;
-  }
-`
+const FooterBox = styled.div(props => ({
+  // @media (max-width: '700px') {
+  //   bottom: 0;
+  //   padding: 5% 0 0;
+  //   margin: 0 -8% -5% -8%;
+  //   position: absolute;
+  //   width: 100%;
+  //   box-sizing: border-box;
+  //   background-color: #eeeeee;
+  // }
+}));
 
-const Footer = () => (
-  <FooterBox>
+
+const Footer = ({footerClass}) => (
+  <div className={`defaultFooter ${footerClass}`}>
     <p>View source on <a href="https://github.com/rashmiap/personal-website-react">Github</a> | Designed by <a href="/">Natasha G</a></p>
-  </FooterBox>
+  </div>
 )
-
+Footer.propTypes = {
+  footerClass: PropTypes.string,
+}
 export default Footer

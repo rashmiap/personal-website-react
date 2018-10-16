67b840c3f3c48d758b59d5a23ce8a5d3739a11a7
Rashmi <rashmiap.10@gmail.com>
2018-10-16 08:54:06 +0530

    json data work lang interests edu added company logo added

---
+++ src/data/Edu.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        id: 1,
+        name: "B M S College of Engineering",
+        major: "Computer Science",
+        degree: "Bachelor",
+        dated: "2011 - 2015",
+        cgpa: "9.12"
+    },
+];

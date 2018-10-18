dfcf1a64205bb9f67ef1294f0857a62566e5ebea
Rashmi <rashmiap.10@gmail.com>
2018-10-18 16:14:35 +0530

    google fonts gatsby plugin added signika font added

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Signika`,
+          `source sans pro\:300,400,600` // you can also specify font weights and styles
+        ]
+      }
+    }
   ],
 }

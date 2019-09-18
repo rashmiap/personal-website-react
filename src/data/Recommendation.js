97aeae189b1ea296a7e0cd0bed659d113f35f3d5
rashmiap <rashmiap.10@gmail.com>
2019-09-18 16:16:15 +0530

    added quotes for rec

--- src/data/Recommendation.js
+++ src/data/Recommendation.js
@@ -5,7 +5,7 @@ export default [
         designation: "Founder and CEO, Zoojoo.BE",
         userLink: "https://www.linkedin.com/in/avinashsaurabh/",
         dated: "January 08, 2019",
-        description: "Rashmi has been one of our best hires for the engineering team. She joined us as an intern and quickly ramped up to independently manage her tasks. She is fast learner, passionate about her work & keeps herself accountable for timely and high quality deliverables - an asset to any team that she works with."
+        description: "\"Rashmi has been one of our best hires for the engineering team. She joined us as an intern and quickly ramped up to independently manage her tasks. She is fast learner, passionate about her work & keeps herself accountable for timely and high quality deliverables - an asset to any team that she works with.\""
     },
     {
         id: 2,
@@ -13,7 +13,7 @@ export default [
         designation: "Integrated Marketing Manager, Zoojoo.BE",
         userLink: "https://www.linkedin.com/in/suvasini-raghavan/",
         dated: "January 20, 2019",
-        description: "I have known Rashmi since the time she joined as an intern. Rashmi worked as a web developer and is extremely quick in understanding and delivering the requirements. Chatty, fun-loving, dedicated, passionate and talented - she was everything that makes for a great colleague!"
+        description: "\"I have known Rashmi since the time she joined as an intern. Rashmi worked as a web developer and is extremely quick in understanding and delivering the requirements. Chatty, fun-loving, dedicated, passionate and talented - she was everything that makes for a great colleague!\""
     },
     {
         id: 3,
@@ -21,7 +21,7 @@ export default [
         designation: "UX Designer, Aware",
         userLink: "https://www.linkedin.com/in/snehanjan/",
         dated: "March 2, 2019",
-        description: "Rashmi has been a great asset to the team. Her keen eye for design and quick ability to learn a technology that is best suited for the task at hand meant quick project turn arounds with a phenomenal output. Any team that is dynamic enough to utilize her skills and capabilities would be lucky to have Rashmi as a member. Also, her caring and Dog-loving nature made her a great person to have around in the office. "
+        description: "\"Rashmi has been a great asset to the team. Her keen eye for design and quick ability to learn a technology that is best suited for the task at hand meant quick project turn arounds with a phenomenal output. Any team that is dynamic enough to utilize her skills and capabilities would be lucky to have Rashmi as a member. Also, her caring and Dog-loving nature made her a great person to have around in the office. \""
     },
     {
         id: 4,
@@ -29,7 +29,7 @@ export default [
         designation: "Software Engineer at RoundGlass",
         userLink: "https://www.linkedin.com/in/princealikhan/",
         dated: "January 14, 2018",
-        description: "Rashmi is very passionate and has a great vision for her work. I work with Rashmi in Zoojoo.Be for past two years. Her focus keeps everything moving smoothly. She is responsible for web interface of Zoojoo.Be product. She makes sure all the deadline are met and at the same time meets the highest standards. Rashmi spends decent amount of time updating her skill and learn new technologies and implement the same to make the product more efficient and user-friendly."
+        description: "\"Rashmi is very passionate and has a great vision for her work. I work with Rashmi in Zoojoo.Be for past two years. Her focus keeps everything moving smoothly. She is responsible for web interface of Zoojoo.Be product. She makes sure all the deadline are met and at the same time meets the highest standards. Rashmi spends decent amount of time updating her skill and learn new technologies and implement the same to make the product more efficient and user-friendly.\""
     },
     {
         id: 5,
@@ -37,6 +37,6 @@ export default [
         designation: "Growth Product Manager at Great Learning",
         userLink: "https://www.linkedin.com/in/prashannaguru/",
         dated: "November 15, 2016",
-        description: "Rashmi is very dedicated and gets things done on time. She owns her work and even goes beyond her responsibility to contribute to the product. She also keeps herself updated about new technologies and implements the same to create a fantastic User interface."
+        description: "\"Rashmi is very dedicated and gets things done on time. She owns her work and even goes beyond her responsibility to contribute to the product. She also keeps herself updated about new technologies and implements the same to create a fantastic User interface.\""
     }
 ];

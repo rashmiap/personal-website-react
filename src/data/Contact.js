bb69f9d7704a1235b247b06016144c5c19a20045
rashmiap <rashmiap.10@gmail.com>
2019-09-18 16:16:03 +0530

    removed phone number

--- src/data/Contact.js
+++ src/data/Contact.js
@@ -4,12 +4,6 @@ import gmail from '../assets/images/gmail.svg'
 export default [
     {
         id: 1,
-        title: "+91 9844869611",
-        icon: phone,
-        linkTo: false,
-    },
-    {
-        id: 2,
         title: "rashmiap.10@gmail.com",
         icon: gmail,
         linkTo: false,

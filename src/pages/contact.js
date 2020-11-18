b294833152ab87e0ac5e08282736339db295d0fa
Rashmi AP <rashmiap.10@gmail.com>
2020-11-18 20:40:28 +0530

    Updated contact layout to single box

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,30 +1,31 @@
-import React from 'react'
-import SocialContact from '../components/Contact/SocialContact'
-import contact from '../data/Contact'
-import social from '../data/Social'
-import { ContactWrapper, ContactHeader, ContactDetails, ContactBox } from '../styles/contactStyle.js'
-import Layout from '../components/layout'
+import React from "react";
+import SocialContact from "../components/Contact/SocialContact";
+import contact from "../data/Contact";
+import social from "../data/Social";
+import {
+	ContactWrapper,
+	ContactHeader,
+	ContactDetails,
+	ContactBox,
+} from "../styles/contactStyle.js";
+import Layout from "../components/layout";
 
 const Contact = () => (
-  <Layout>
-  <ContactWrapper>
-    <ContactHeader>Get in touch</ContactHeader>
-    <ContactDetails>
-        <ContactBox>
-          <h3>Contact Details</h3>
-          {
-            contact.map(item => (<SocialContact key={item.id} info={item} />))
-          }
-        </ContactBox>
-        <ContactBox>
-          <h3>Look me up online</h3>
-          {
-            social.map(item => (<SocialContact key={item.id} info={item} />))
-          }
-        </ContactBox>
-    </ContactDetails>
-  </ContactWrapper>
-  </Layout>
-)
+	<Layout>
+		<ContactWrapper>
+			<ContactHeader>Get in touch</ContactHeader>
+			<ContactDetails>
+				<ContactBox>
+					{contact.map((item) => (
+						<SocialContact key={item.id} info={item} />
+					))}
+					{social.map((item) => (
+						<SocialContact key={item.id} info={item} />
+					))}
+				</ContactBox>
+			</ContactDetails>
+		</ContactWrapper>
+	</Layout>
+);
 
-export default Contact
+export default Contact;
--- src/styles/contactStyle.js
+++ src/styles/contactStyle.js
@@ -1,37 +1,32 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const ContactWrapper = styled.div`
-  margin: 10% auto;
-  @media (max-width: 700px) {
-    margin: 15% auto;
-  }
+	margin: 10% auto;
+	@media (max-width: 700px) {
+		margin: 15% auto;
+	}
 `;
 export const ContactHeader = styled.h1`
-  text-align: CENTER;
-  color: #eeeeee;
-  margin-bottom: 5%;
-  font-weight: 300;
-`
+	text-align: CENTER;
+	color: #7fa1e8;
+	margin-bottom: 5%;
+	font-weight: 300;
+`;
 export const ContactDetails = styled.div`
-  display: flex;
-  align-items: baseline;
-  justify-content: space-around;
-  h3{
-    font-size: 30px;
-    color: #7fa1e8;
-    font-weight: 300;
-  }
-  @media (max-width: 700px) {
-    flex-direction: column;
-    align-items: center;
-  }
-`
+	display: flex;
+	align-items: baseline;
+	justify-content: center;
+	@media (max-width: 700px) {
+		flex-direction: column;
+		align-items: center;
+	}
+`;
 export const ContactBox = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  font-weight: 300;
-  @media (max-width: 700px) {
-    flex-direction: column;
-  }
-`
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	font-weight: 300;
+	@media (max-width: 700px) {
+		flex-direction: column;
+	}
+`;

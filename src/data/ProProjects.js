a5d2a53784e60a672375be03f23b4da8382b3129
Rashmi ap <rashmiap.10@gmail.com>
2021-03-30 16:47:31 +0530

    small change

--- src/data/ProProjects.js
+++ src/data/ProProjects.js
@@ -1,103 +1,111 @@
-import pregnancy from '../assets/images/projects/pregnancy.svg'
-import like from '../assets/images/projects/social.svg'
-import dare from '../assets/images/projects/dare.svg'
-import journey from '../assets/images/projects/journey.svg'
-import website from '../assets/images/projects/website.svg'
-import aware from '../assets/images/projects/aware.svg'
-import email from '../assets/images/projects/email.svg'
-import vision from '../assets/images/projects/vision.svg'
-import search from '../assets/images/projects/search.svg'
+import pregnancy from "../assets/images/projects/pregnancy.svg";
+import like from "../assets/images/projects/social.svg";
+import dare from "../assets/images/projects/dare.svg";
+import journey from "../assets/images/projects/journey.svg";
+import website from "../assets/images/projects/website.svg";
+import aware from "../assets/images/projects/aware.svg";
+import email from "../assets/images/projects/email.svg";
+import vision from "../assets/images/projects/vision.svg";
+import search from "../assets/images/projects/search.svg";
 
 export default [
-    {
-        id: 9,
-        title: "Email ERA",
-        icon: email,
-        description: "Email ERA is an intelligent, self-learning end-to-end mail management tool that bridges the gap between mailbox and CRM using NLP capabilities. It reads through e-mails, processes content, categorizes them as per business use cases and generates auto-response suggestions to the majority emails. Developed a mobile responsive front-end application with ReactJS, Material-UI for CSS and MobX for state management. Also, the web application leverages the usage of WebSockets to fetch real-time email conversations.",
-        githubPath: null,
-        demoPath: null,
-        year: "2019",
-        techUsed: "React, Material-UI, MobX, WebSockets"
-    },
-    {
-        id: 8,
-        title: "Knowledge Base Bot",
-        icon: search,
-        description: "Knowledge Base portal is used to create, organize, search and share any content such as pdf, docs, images, videos and more internally by admins and agents of an enterprise. Some features of the platform include versioning documents, designing quizzes for a document and generating built-in reports for quizzes, documents and agents activity. Developed the front-end of the portal in ReactJS using Material-UI for CSS and MobX for state management.",
-        githubPath: null,
-        demoPath: null,
-        year: "2019",
-        techUsed: "React, Material-UI, MobX"
-    },
-    {
-        id: 7,
-        title: "Vision ERA OCR",
-        icon: vision,
-        description: "Vision ERA platform uses OCR  technologies to extract relevant data from printed documents such as government ID cards and invoices while automating a significant amount of cognitive work. Developed the front-end using HTML5 Canvas with ReactJS, Material-UI for CSS and MobX for state management. The uploaded document is mapped onto the canvas which shows the highlighted co-ordinates by hovering over the extracted data and also allowing users to extract a snippet by selecting the mouse over the area.",
-        githubPath: null,
-        demoPath: null,
-        year: "2019",
-        techUsed: "React, Material-UI, MobX"
-    },
-    {
-        id: 6,
-        title: "Zoojoobe Social Stream",
-        icon: like,
-        description: "Social feed is a feature on the Zoojoobe platform that leverages on the social behaviour within the corporate.It allows users on the platform to share media that keeps them motivated to do a certain task. Like all other social feeds, Zoojoobe Stream also has social elements like liking, sharing, commenting, subscribing to a particular post and so on. Developed using ReactJS and draft-js-plugins, a react plugin architecture for Facebook's Draft.js which includes emojis, mentions and stickers. ",
-        githubPath: null,
-        demoPath: null,
-        year: "2018",
-        techUsed: "React, react-dropzone, draft-js-plugins"
-    },
-    {
-        id: 5,
-        title: "Zoojoobe Dare",
-        icon: dare,
-        description: "Dare is a feature on the Zoojoobe platform developed in ReactJS that primarily focuses on the competitive behaviour of the users. Two users compete with each other to complete a particular healthy task. Meanwhile other users within the corporate can motivate the user using the cheer function.",
-        githubPath: null,
-        demoPath: null,
-        year: "2017",
-        techUsed: "React, Material-UI"
-    },
-    {
-        id: 4,
-        title: "Zoojoobe Journey",
-        icon: journey,
-        description: "Journey feature on the Zoojoobe platform is a set of curated activities that are a set of micro-tasks that a user has to form to achieve a bigger goal like forming a complex habit. Journey feature has multiple levels which changes based on user activity. The whole task list is customised based on user activity.",
-        githubPath: null,
-        demoPath: null,
-        year: "2017",
-        techUsed: "React, Material-UI"
-    },
-    {
-        id: 3,
-        title: "Zoojoobe Pregnancy module",
-        icon: pregnancy,
-        description: "Maternity module was primarily designed for the women within a corporate. The feature takes into account the user's due date based on which it recommends articles and relevant mindfulness tracks. This feature helps them stay healthy throughout the process.",
-        githubPath: null,
-        demoPath: null,
-        year: "2017",
-        techUsed: "React, Material-UI, React-Sound-Manager"
-    },
-    {
-        id: 2,
-        title: "Zoojoobe Sales website",
-        icon: website,
-        description: "Zoojoobe is a game based social wellness platform that unites the workforce to form healthier habits. The platform leverages on the tremendous power of the trusted social network at our workplace to motivate employees to form healthier habits. Developed the sales website on wordpress.",
-        githubPath: null,
-        demoPath: "https://www.zoojoo.be",
-        year: "2016",
-        techUsed: "HTML, CSS, jquery"
-    },
-    {
-        id: 1,
-        title: "Aware Dashboard",
-        icon: aware,
-        description: "Aware is a mindfulness meditation mobile app which provides guided meditation sessions to users. Developed the web front end for Aware Dashboard using ReactJS. Some of the features of this dashboard include User Profile, App related stats, Coupon redemption, Gifting module and Payment gateway integration using Paddle.",
-        githubPath: null,
-        demoPath: "https://my.awaremeditationapp.com/",
-        year: "2017",
-        techUsed: "React, Material-UI, SASS"
-    },
-
+	{
+		id: 9,
+		title: "Email ERA",
+		icon: email,
+		description:
+			"Email ERA is an intelligent, self-learning end-to-end mail management tool that bridges the gap between mailbox and CRM using NLP capabilities. It reads through e-mails, processes content, categorizes them as per business use cases and generates auto-response suggestions to the majority emails. Developed a mobile responsive front-end application with ReactJS, Material-UI for CSS and MobX for state management. Also, the web application leverages the usage of WebSockets to fetch real-time email conversations.",
+		githubPath: null,
+		demoPath: null,
+		year: "2019",
+		techUsed: "React, Material-UI, MobX, WebSockets",
+	},
+	{
+		id: 8,
+		title: "Knowledge Base Bot",
+		icon: search,
+		description:
+			"Knowledge Base portal is used to create, organize, search and share any content such as pdf, docs, images, videos and more internally by admins and agents of an enterprise. Some features of the platform include versioning documents, designing quizzes for a document and generating built-in reports for quizzes, documents and agents activity. Developed the front-end of the portal in ReactJS using Material-UI for CSS and MobX for state management.",
+		githubPath: null,
+		demoPath: null,
+		year: "2019",
+		techUsed: "React, Material-UI, MobX",
+	},
+	{
+		id: 7,
+		title: "Vision ERA OCR",
+		icon: vision,
+		description:
+			"Vision ERA platform uses OCR  technologies to extract relevant data from printed documents such as government ID cards and invoices while automating a significant amount of cognitive work. Developed the front-end using HTML5 Canvas with ReactJS, Material-UI for CSS and MobX for state management. The uploaded document is mapped onto the canvas which shows the highlighted co-ordinates by hovering over the extracted data and also allowing users to extract a snippet by selecting the mouse over the area.",
+		githubPath: null,
+		demoPath: null,
+		year: "2019",
+		techUsed: "React, Material-UI, MobX",
+	},
+	{
+		id: 6,
+		title: "Zoojoobe Social Stream",
+		icon: like,
+		description:
+			"Social feed is a feature on the Zoojoobe platform that leverages on the social behaviour within the corporate.It allows users on the platform to share media that keeps them motivated to do a certain task. Like all other social feeds, Zoojoobe Stream also has social elements like liking, sharing, commenting, subscribing to a particular post and so on. Developed using ReactJS and draft-js-plugins, a react plugin architecture for Facebook's Draft.js which includes emojis, mentions and stickers. ",
+		githubPath: null,
+		demoPath: null,
+		year: "2018",
+		techUsed: "React, react-dropzone, draft-js-plugins",
+	},
+	{
+		id: 5,
+		title: "Zoojoobe Dare",
+		icon: dare,
+		description:
+			"Dare is a feature on the Zoojoobe platform developed in ReactJS that primarily focuses on the competitive behaviour of the users. Two users compete with each other to complete a particular healthy task. Meanwhile other users within the corporate can motivate the user using the cheer function.",
+		githubPath: null,
+		demoPath: null,
+		year: "2017",
+		techUsed: "React, Material-UI",
+	},
+	{
+		id: 4,
+		title: "Zoojoobe Journey",
+		icon: journey,
+		description:
+			"Journey feature on the Zoojoobe platform is a set of curated activities that are a set of micro-tasks that a user has to form to achieve a bigger goal like forming a complex habit. Journey feature has multiple levels which changes based on user activity. The whole task list is customised based on user activity.",
+		githubPath: null,
+		demoPath: null,
+		year: "2017",
+		techUsed: "React, Material-UI",
+	},
+	{
+		id: 3,
+		title: "Zoojoobe Pregnancy module",
+		icon: pregnancy,
+		description:
+			"Maternity module was primarily designed for the women within a corporate. The feature takes into account the user's due date based on which it recommends articles and relevant mindfulness tracks. This feature helps them stay healthy throughout the process.",
+		githubPath: null,
+		demoPath: null,
+		year: "2017",
+		techUsed: "React, Material-UI, React-Sound-Manager",
+	},
+	{
+		id: 2,
+		title: "Zoojoobe Sales website",
+		icon: website,
+		description:
+			"Zoojoobe is a game based social wellness platform that unites the workforce to form healthier habits. The platform leverages on the tremendous power of the trusted social network at our workplace to motivate employees to form healthier habits. Developed the sales website on wordpress.",
+		githubPath: null,
+		demoPath: "https://www.zoojoo.be",
+		year: "2016",
+		techUsed: "Jquery, HTML, CSS",
+	},
+	{
+		id: 1,
+		title: "Aware Dashboard",
+		icon: aware,
+		description:
+			"Aware is a mindfulness meditation mobile app which provides guided meditation sessions to users. Developed the web front end for Aware Dashboard using ReactJS. Some of the features of this dashboard include User Profile, App related stats, Coupon redemption, Gifting module and Payment gateway integration using Paddle.",
+		githubPath: null,
+		demoPath: "https://my.awaremeditationapp.com/",
+		year: "2017",
+		techUsed: "React, Material-UI, SASS",
+	},
 ];
